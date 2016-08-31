7301a8211249ccb022cd3a5433c863202f28de8c
Ronnypetson <rsronnypetson4@gmail.com>
2016-08-30 20:33:11 -0500

    Add files via upload

---
+++ Negocio/Votacao.java
@@ -0,0 +1,109 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mineracaodadoseleitorais;
+
+/**
+ *
+ * @author ronnypetsonss
+ */
+public class Votacao {
+    protected String AnoEleicao;
+    protected String NumTurno;
+    protected String DescricaoEleicao;
+    protected String SiglaUF;
+    protected String SigaUE;
+    protected String CodigoMunicipio;
+    protected String NomeMunicipio;
+    protected String CodigoCargo;
+    protected String DescricaoCargo;
+    
+    public Votacao(){
+    }
+
+    public Votacao(String[] entry) {
+        this.AnoEleicao = entry[0];
+        this.NumTurno = entry[1];
+        this.DescricaoEleicao = entry[2];
+        this.SiglaUF = entry[3];
+        this.SigaUE = entry[4];
+        this.CodigoMunicipio = entry[5];
+        this.NomeMunicipio = entry[6];
+        this.CodigoCargo = entry[7];
+        this.DescricaoCargo = entry[8];
+    }
+    
+    public String getAnoEleicao() {
+        return AnoEleicao;
+    }
+
+    public void setAnoEleicao(String AnoEleicao) {
+        this.AnoEleicao = AnoEleicao;
+    }
+
+    public String getNumTurno() {
+        return NumTurno;
+    }
+
+    public void setNumTurno(String NumTurno) {
+        this.NumTurno = NumTurno;
+    }
+
+    public String getDescricaoEleicao() {
+        return DescricaoEleicao;
+    }
+
+    public void setDescricaoEleicao(String DescricaoEleicao) {
+        this.DescricaoEleicao = DescricaoEleicao;
+    }
+
+    public String getSiglaUF() {
+        return SiglaUF;
+    }
+
+    public void setSiglaUF(String SiglaUF) {
+        this.SiglaUF = SiglaUF;
+    }
+
+    public String getSigaUE() {
+        return SigaUE;
+    }
+
+    public void setSigaUE(String SigaUE) {
+        this.SigaUE = SigaUE;
+    }
+
+    public String getCodigoMunicipio() {
+        return CodigoMunicipio;
+    }
+
+    public void setCodigoMunicipio(String CodigoMunicipio) {
+        this.CodigoMunicipio = CodigoMunicipio;
+    }
+
+    public String getNomeMunicipio() {
+        return NomeMunicipio;
+    }
+
+    public void setNomeMunicipio(String NomeMunicipio) {
+        this.NomeMunicipio = NomeMunicipio;
+    }
+
+    public String getCodigoCargo() {
+        return CodigoCargo;
+    }
+
+    public void setCodigoCargo(String CodigoCargo) {
+        this.CodigoCargo = CodigoCargo;
+    }
+
+    public String getDescricaoCargo() {
+        return DescricaoCargo;
+    }
+
+    public void setDescricaoCargo(String DescricaoCargo) {
+        this.DescricaoCargo = DescricaoCargo;
+    }
+}
---
+++ Negocio/Votavel.java
@@ -0,0 +1,89 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mineracaodadoseleitorais;
+
+/**
+ *
+ * @author ronnypetsonss
+ */
+public class Votavel {
+    protected String AnoEleicao;
+    protected String NumTurno;
+    protected String SiglaUF;
+    protected String SiglaUE;
+    protected String NomeUE;
+    protected String CodigoCargo;
+    protected String DescricaoCargo;
+
+    public Votavel() {
+    }
+
+    public Votavel(String[] entry) {
+        this.AnoEleicao = entry[0];
+        this.NumTurno = entry[1];
+        this.SiglaUF = entry[2];
+        this.SiglaUE = entry[3];
+        this.NomeUE = entry[4];
+        this.CodigoCargo = entry[5];
+        this.DescricaoCargo = entry[6];
+    }
+
+    public String getAnoEleicao() {
+        return AnoEleicao;
+    }
+
+    public void setAnoEleicao(String AnoEleicao) {
+        this.AnoEleicao = AnoEleicao;
+    }
+
+    public String getNumTurno() {
+        return NumTurno;
+    }
+
+    public void setNumTurno(String NumTurno) {
+        this.NumTurno = NumTurno;
+    }
+
+    public String getSiglaUF() {
+        return SiglaUF;
+    }
+
+    public void setSiglaUF(String SiglaUF) {
+        this.SiglaUF = SiglaUF;
+    }
+
+    public String getSiglaUE() {
+        return SiglaUE;
+    }
+
+    public void setSiglaUE(String SiglaUE) {
+        this.SiglaUE = SiglaUE;
+    }
+
+    public String getNomeUE() {
+        return NomeUE;
+    }
+
+    public void setNomeUE(String NomeUE) {
+        this.NomeUE = NomeUE;
+    }
+
+    public String getCodigoCargo() {
+        return CodigoCargo;
+    }
+
+    public void setCodigoCargo(String CodigoCargo) {
+        this.CodigoCargo = CodigoCargo;
+    }
+
+    public String getDescricaoCargo() {
+        return DescricaoCargo;
+    }
+
+    public void setDescricaoCargo(String DescricaoCargo) {
+        this.DescricaoCargo = DescricaoCargo;
+    }
+}
