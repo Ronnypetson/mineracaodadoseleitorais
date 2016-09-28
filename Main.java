5a206f43fed04a10b5c29a04140274c5d1c1a76f
Ronnypetson <rsronnypetson4@gmail.com>
2016-09-28 02:11:09 -0300

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,20 +1,25 @@
 package mineracaodadoseleitorais;
 
-import mineracaodadoseleitorais.DAO.BemDeCandidatoFileTableReader;
 import java.io.IOException;
 import java.sql.SQLException;
-// import java.util.Scanner;
+import java.util.ArrayList;
 import mineracaodadoseleitorais.DAO.DAOTSE;
+import mineracaodadoseleitorais.negocio.Candidatura;
+import mineracaodadoseleitorais.negocio.PerfilEleitor;
+import mineracaodadoseleitorais.negocio.VotacaoCandidato;
 
 public class Main {
 
-	public static void main(String[] args) throws ClassNotFoundException, InstantiationException, IllegalAccessException, SQLException, IOException {
-                // DAO testing
-                DAOTSE myDAO = new DAOTSE();
-                myDAO.connect();
-                // myDAO.clearTable("PerfilEleitor");
-                // myDAO.insertAllVotacaoSecao();
-                myDAO.printGet("VotacaoSecao");
-                myDAO.disconnect();
-	}
+    public static void main(String[] args)
+            throws ClassNotFoundException, InstantiationException,
+            IllegalAccessException, SQLException, IOException {
+        // DAO testing
+        DAOTSE myDAO = new DAOTSE();
+        myDAO.connect();
+        // myDAO.clearTable("VotacaoCandidato");
+        // myDAO.insertAllVotacaoCandidato();
+        // myDAO.printGet("VotacaoSecao");
+        ArrayList<Candidatura> perfis = myDAO.getPerfisCandidaturasSecao("56");
+        myDAO.disconnect();
+    }
 }
